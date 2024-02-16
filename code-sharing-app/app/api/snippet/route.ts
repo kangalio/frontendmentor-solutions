cf1f9ed371ac9175ed863cf8fc01a254f6432d33
kangalioo <jannik.a.schaper@web.de>
2024-02-16 23:16:02 +0100

    Sigh, GHA debugging flashbacks

--- code-sharing-app/app/api/snippet/route.ts
+++ code-sharing-app/app/api/snippet/route.ts
@@ -1,8 +1,12 @@
-import { writeFile } from "fs/promises";
+import { writeFile, mkdir } from "fs/promises";
+import path from "path";
 
 export async function POST(request: Request) {
+  let directory = "snippets";
   let filename = new Date().toISOString();
-  writeFile(`snippets/${filename}`, await request.text());
+
+  mkdir(directory, { recursive: true });
+  writeFile(path.join(directory, filename), await request.text());
 
   return Response.json({
     url: `${request.headers.get("Host") ?? "localhost:3000"}?id=${filename}`,
